213b4b414859a2fc5a8e6648675899c9d4c221ea
lucasvt22 <lucas22@vt.edu>
2026-04-27 16:51:27 -0400

    Add create connection for db

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+const db = mysql.createConnection(
+  process.env.MYSQL_URL || {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  }
+);
 
 db.connect((err) => {
   if (err) {
